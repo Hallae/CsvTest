1502e1d4af36faa586c878ef0bdcfa880c011ded
Hallae <mr.tabby@hotmail.com>
2024-04-14 00:57:49 +0300

    Add project files.

---
+++ Models/Organization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvTest
+{
+    public class Organization
+    {
+        public string Name { get; set; } = "Orbiters";
+        public string LegalAddress { get; set; } = "123 Main St, Anytown, USA";
+        public string ActualAdress { get; set; } = "123 Main St, Anytown, USA";
+    }
+}
