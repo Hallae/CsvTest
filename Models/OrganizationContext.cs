e3d62e7f9ba0fbde84468c6df446425f4355c274
Hallae <mr.tabby@hotmail.com>
2024-04-14 01:06:43 +0300

    Adde:Models/classes

---
+++ Models/OrganizationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvTest.Models
+{
+    public class OrganizationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("OrganizationConnection");
+        }
+        public DbSet<Organization> Organization { get; set; }
+
+    }
+}
